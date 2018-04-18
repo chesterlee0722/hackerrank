ed2eaa1c63425ed028fc6faf3a4b32fec444db18
chesterli <sunnylyme@163.com>
2018-04-18 11:30:27 +0800

    Commit solution of [Grading Students]

---
+++ challenges/grading/Grading Students.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/grading/problem
+
+import os
+import sys
+
+#
+# Complete the gradingStudents function below.
+#
+def calculateGrade(grade):
+    print(grade, grade % 5)
+    if(grade < 38):
+        return grade
+    elif((grade % 5 >= 3) and (grade % 5 != 0)):
+        return 5 *(int(int(grade) / 5) + 1)
+    return grade
+
+def gradingStudents(grades):
+    #
+    # Write your code here.
+    #
+    return map(calculateGrade, grades)
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+    
+    n = int(input())
+    
+    grades = []
+    
+    for _ in range(n):
+        grades_item = int(input())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+f.write('\n'.join(map(str, result)))
+f.write('\n')
+
+f.close()
